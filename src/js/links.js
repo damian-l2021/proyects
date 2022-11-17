4ab2ea69b3592b50d0134a6ec0d8a32a07be254d
Damian <damian_l@hotmail.es>
2022-11-17 11:20:39 -0300

    first-commit

---
+++ src/js/links.js
@@ -0,0 +1,16 @@
+
+
+ export const links = [
+        {
+            "name": "Producciones",
+            "link": "/producciones"
+        },
+        {
+            "name": "Precios",
+            "link": "/precios"
+        },
+        {
+            "name": "Contacto",
+            "link": "/contacto"
+        }
+    ]
